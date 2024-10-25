733f4962d6215e0415fd84d5daf8ed2ed315daa9
unknown <wt.vathanh@gmail.com>
2024-10-25 16:23:22 +0700

    use name instead of last and first naem for founder tabel

--- backend/startup/models.py
+++ backend/startup/models.py
@@ -10,8 +10,7 @@ class Category(models.Model):
 
 class Founder(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
 
     def __str__(self):
