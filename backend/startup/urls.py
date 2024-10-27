b4c85980d6bebbd5322f6bf48a7520aa707669f6
minimonomana <ductrunga1k48pbc@gmail.com>
2024-10-27 18:04:13 +0700

    Update url

--- backend/startup/urls.py
+++ backend/startup/urls.py
@@ -2,5 +2,8 @@ from django.urls import path , include
 
 urlpatterns = [
     path('startups/', include('startup.api.startup.urls')),
+    path('batch/', include('startup.api.batch.urls')),
+    path('categories/', include('startup.api.category.urls')),
+    path('founders/', include('startup.api.founder.urls')),
 ]
 
