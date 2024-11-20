195f680aa244cde965ecc814c618c0ae463b94e2
WuTan-Thanh <wt.vatanh@gmail.com>
2024-11-20 14:16:52 +0000

    setup minio

--- backend/core/avatar/service.py
+++ backend/core/avatar/service.py
@@ -1,6 +1,7 @@
 import random
 import string
 import os
+import re
 
 from ..models.avatar_model import Avatar
 from django.core.files.uploadedfile import UploadedFile
@@ -18,6 +19,18 @@ minio_client = Minio(
 BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME", "notset")
 FOLDER_NAME = "avatars"
 
+def sanitize_filename(filename: str) -> str:
+    """
+    Remove all characters from the filename except a-z, A-Z, 0-9, and _.
+    
+    Args:
+        filename (str): The original filename.
+    
+    Returns:
+        str: The sanitized filename.
+    """
+    return re.sub(r'[^a-zA-Z0-9_.]', '',filename)
+
 def generate_random_string(length=20) -> str:
     """_summary_
 
@@ -29,10 +42,9 @@ def generate_random_string(length=20) -> str:
     """
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
-
 def upload_avatar(file: UploadedFile) -> Avatar:
     """
-    Upload a new avatar image to minio and create a new Avatar instance.
+    Upload a new avatar image to MinIO and create a new Avatar instance.
     
     Args:
         file (UploadedFile): The file to be saved in the Avatar instance.
@@ -40,12 +52,16 @@ def upload_avatar(file: UploadedFile) -> Avatar:
     Returns:
         Avatar: The created Avatar instance.
     """
-    # check if bucket exists
+    # Check if bucket exists
     if not minio_client.bucket_exists(BUCKET_NAME):
         raise ValidationError("Bucket does not exist.")
     
-    # generate a random string name for the file: random_string that include both numbers and letters 20 characters long
-    file.name = f"{generate_random_string()}_avatar_{file.name}"
+    # Sanitize the filename
+    sanitized_name = sanitize_filename(file.name)
+    
+    # Generate a random string name for the file
+    file.name = f"{generate_random_string()}_avatar_{sanitized_name}"
+    
     try:
         # Save the file to MinIO
         minio_client.put_object(
@@ -60,7 +76,7 @@ def upload_avatar(file: UploadedFile) -> Avatar:
     except Exception as e:
         raise ValidationError(f"Error uploading file: {str(e)}")
     
-    avatar_url = f"{os.getenv('MINIO_URL', 'localhost:9000')}/{BUCKET_NAME}/{FOLDER_NAME}/{file.name}"
+    avatar_url = f"{os.getenv('FILE_MINIO_URL', 'localhost:9000')}/{BUCKET_NAME}/{FOLDER_NAME}/{file.name}"
     avatar = Avatar(avatar_url=avatar_url)
     avatar.save()  # Save the instance to trigger any custom save logic if needed
     return avatar
